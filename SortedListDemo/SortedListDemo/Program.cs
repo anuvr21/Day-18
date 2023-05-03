a2f7a16fb3d184b83df380d0c9b57931e05e5e9d
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 01:55:42 +0530

    Add files via upload

---
+++ SortedListDemo/SortedListDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortedListDemo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList s = new SortedList();
+            s.Add("Maharashtra", "Mumbai");
+            s.Add("Karnataka", "Bangalore");
+            s.Add("Andhra Pradesh", "Hyderabad");
+            Console.WriteLine("Elements in the sorted list");
+            foreach(var item in s.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in s.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ SortedListDemo2/SortedListDemo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedListDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList s = new SortedList();
+            s.Add("Maharastra", "Mumbai");
+            s.Add("Andhra Pradesh", "Hyderabad");
+            s.Add("Tamil Nadu","Chennai");
+            s.Add("Karnataka","Bangalore");
+            Console.WriteLine("Elements in the sorted list");
+            IDictionaryEnumerator e=s.GetEnumerator();
+            while(e.MoveNext())
+            {
+                Console.WriteLine(e.Key+"\t"+e.Value);
+            }
+
+
+        }
+    }
+}
